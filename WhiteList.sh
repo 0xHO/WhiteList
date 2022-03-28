052f6086a9393cd20ad392b58f24edf670d34a1f
0xHO <93760897+0xHO@users.noreply.github.com>
2022-03-28 22:18:17 +0800

    Update WhiteList.sh

--- WhiteList.sh
+++ WhiteList.sh
@@ -10,37 +10,37 @@ function getdomain () {
     echo $include
     echo "! # ------ ${include} ------" >> ./WhiteList.tmp
     # no full no # domains
-    domainlist=`cat ./data/${include}|grep -v "#"|grep -v "^$"|grep -v "include:"|grep -v "full:"|cut -d'@' -f 1`
+    domainlist=`cat ./domainlist/data/${include}|grep -v "#"|grep -v "^$"|grep -v "include:"|grep -v "full:"|cut -d'@' -f 1`
     for domain in ${domainlist}
     do
         echo "||${domain}" >> ./WhiteList.tmp
     done
     # no full # domains
-    domainlist=`cat ./data/${include}|grep "#"|grep -v "^#"|grep -v "include:"|grep -v "full:"|cut -d'#' -f 1`
+    domainlist=`cat ./domainlist/data/${include}|grep "#"|grep -v "^#"|grep -v "include:"|grep -v "full:"|cut -d'#' -f 1`
     for domain in ${domainlist}
     do
         echo "||${domain}" >> ./WhiteList.tmp
     done
     # full  no # domains
-    domainlist=`cat ./data/${include}|grep -v "include:"|grep "full:"|grep -v "#"|cut -d':' -f 2`
+    domainlist=`cat ./domainlist/data/${include}|grep -v "include:"|grep "full:"|grep -v "#"|cut -d':' -f 2`
     for domain in ${domainlist}
     do
         echo "||${domain}" >> ./WhiteList.tmp
     done
     # full  # domains
-    domainlist=`cat ./data/${include}|grep -v "include:"|grep "full:" |grep "#"|cut -d':' -f 2 |cut -d'#' -f 1`
+    domainlist=`cat ./domainlist/data/${include}|grep -v "include:"|grep "full:" |grep "#"|cut -d':' -f 2 |cut -d'#' -f 1`
     for domain in ${domainlist}
     do
         echo "||${domain}" >> ./WhiteList.tmp
     done
     # include file - no#
-    includs=`cat ./data/${include}|grep "include:"|grep -v "#"|cut -d: -f2`
+    includs=`cat ./domainlist/data/${include}|grep "include:"|grep -v "#"|cut -d: -f2`
     for incoude in ${includs}
     do
         getdomain ${incoude}
     done
     # include #
-    includs=`cat ./data/${include}|grep "include:"|grep "#"|cut -d: -f2|cut -d" " -f1`
+    includs=`cat ./domainlist/data/${include}|grep "include:"|grep "#"|cut -d: -f2|cut -d" " -f1`
     for incoude in ${includs}
     do
         getdomain ${incoude}
