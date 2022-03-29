1ec2341d616f7d68862739ca684a28e2f9caa053
0xHO <93760897+0xHO@users.noreply.github.com>
2022-03-29 09:58:55 +0800

    Update WhiteList.sh

--- WhiteList.sh
+++ WhiteList.sh
@@ -8,30 +8,30 @@ function getdomain () {
         include=$1
     fi
     echo $include
-    echo "! # ------ ${include} ------" >> ./WhiteList.tmp
+    echo "! # ------ ${include} ------>" >> ./WhiteList.tmp
     # no full no # domains
     domainlist=`cat ./domainlist/data/${include}|grep -v "#"|grep -v "^$"|grep -v "include:"|grep -v "full:"|cut -d'@' -f 1`
     for domain in ${domainlist}
     do
-        echo "||${domain}" >> ./WhiteList.tmp
+        echo "|| ${domain}" >> ./WhiteList.tmp
     done
     # no full # domains
     domainlist=`cat ./domainlist/data/${include}|grep "#"|grep -v "^#"|grep -v "include:"|grep -v "full:"|cut -d'#' -f 1`
     for domain in ${domainlist}
     do
-        echo "||${domain}" >> ./WhiteList.tmp
+        echo "|| ${domain}" >> ./WhiteList.tmp
     done
     # full  no # domains
     domainlist=`cat ./domainlist/data/${include}|grep -v "include:"|grep "full:"|grep -v "#"|cut -d':' -f 2`
     for domain in ${domainlist}
     do
-        echo "||${domain}" >> ./WhiteList.tmp
+        echo "|| ${domain}" >> ./WhiteList.tmp
     done
     # full  # domains
     domainlist=`cat ./domainlist/data/${include}|grep -v "include:"|grep "full:" |grep "#"|cut -d':' -f 2 |cut -d'#' -f 1`
     for domain in ${domainlist}
     do
-        echo "||${domain}" >> ./WhiteList.tmp
+        echo "|| ${domain}" >> ./WhiteList.tmp
     done
     # include file - no#
     includs=`cat ./domainlist/data/${include}|grep "include:"|grep -v "#"|cut -d: -f2`
@@ -57,7 +57,7 @@ function getheader(){
 ! HomePage: https://github.com/0xHO/WhiteList
 ! License: https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
 !
-! # ------ local network - Regex ------
+! # ------ local network (Regex) ------
 /10\.\d+\.\d+\.\d+/
 /127\.0\.0\.1/
 /172\.16\.\d+\.\d+/
