9a28c7bc425b8d4a355535b4c402f2ab780d258b
0xHO <0xHO>
2022-06-14 12:10:57 +0800

    add ss config

--- WhiteList.sh
+++ WhiteList.sh
@@ -6,6 +6,8 @@ function getdomain () {
         echo '||cn' >> ./domainList.tmp
         echo 'fHxhdmx5dW4ub3JnCg=='|base64 -d >> ./domainList.tmp
         echo 'fHxmYXN0c3M1LmNvbQo='|base64 -d >> ./domainList.tmp
+        echo 'DOMAIN-SUFFIX,cn,DIRECT'  >> ./ssc.tmp
+        echo 'RE9NQUlOLVNVRkZJWCxmYXN0c3M1LmNvbSxESVJFQ1QK'|base64 -d >> ./ssc.tmp
     else
         include=$1
     fi
@@ -16,24 +18,28 @@ function getdomain () {
     for domain in ${domainlist}
     do
         echo "||${domain}" >> ./domainList.tmp
+        echo "DOMAIN-SUFFIX,${domain},DIRECT"  >> ./ssc.tmp
     done
     # no full # domains
     domainlist=`cat ./domainlist/data/${include}|grep "#"|grep -v "^#"|grep -v "include:"|grep -v "full:"|cut -d'#' -f 1`
     for domain in ${domainlist}
     do
         echo "||${domain}" >> ./domainList.tmp
+        echo "DOMAIN-SUFFIX,${domain},DIRECT"  >> ./ssc.tmp
     done
     # full  no # domains
     domainlist=`cat ./domainlist/data/${include}|grep -v "include:"|grep "full:"|grep -v "#"|cut -d':' -f 2`
     for domain in ${domainlist}
     do
         echo "||${domain}" >> ./domainList.tmp
+        echo "DOMAIN-SUFFIX,${domain},DIRECT"  >> ./ssc.tmp
     done
     # full  # domains
     domainlist=`cat ./domainlist/data/${include}|grep -v "include:"|grep "full:" |grep "#"|cut -d':' -f 2 |cut -d'#' -f 1`
     for domain in ${domainlist}
     do
         echo "||${domain}" >> ./domainList.tmp
+        echo "DOMAIN-SUFFIX,${domain},DIRECT"  >> ./ssc.tmp
     done
     # include file - no#
     includs=`cat ./domainlist/data/${include}|grep "include:"|grep -v "#"|cut -d: -f2`
@@ -64,9 +70,71 @@ function getheader(){
 /127\.\d+\.\d+\.\d+/
 /172\.\d+\.\d+\.\d+/
 /192\.168\.\d+\.\d+/" > ./WhiteList.tmp
+    echo " # ssc Last Modified: "`date`"
+# HomePage: https://github.com/0xHO/WhiteList
+# License: https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+[General]
+bypass-system = true
+skip-proxy = 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12, localhost, *.local, *.apple.com
+tun-excluded-routes = 10.0.0.0/8, 100.64.0.0/10, 127.0.0.0/8, 169.254.0.0/16, 172.16.0.0/12, 192.0.0.0/24, 192.0.2.0/24, 192.88.99.0/24, 192.168.0.0/16, 198.51.100.0/24, 203.0.113.0/24, 224.0.0.0/4, 255.255.255.255/32, 239.255.255.250/32
+dns-server =  system
+ipv6 = true
+prefer-ipv6 = false
+dns-fallback-system = false
+dns-direct-system = false
+icmp-auto-reply = false
+always-reject-url-rewrite = false
+private-ip-answer = false
+dns-direct-fallback-proxy = true
+bypass-system = true
+
+[Rule]
+" > ./ssc.tmp
 }
 
 
 getheader
 getdomain
 cat ./domainList.tmp >> ./WhiteList.tmp
+echo "
+# 常见广告域名屏蔽
+DOMAIN-KEYWORD,admarvel,REJECT
+DOMAIN-KEYWORD,admaster,REJECT
+DOMAIN-KEYWORD,adsage,REJECT
+DOMAIN-KEYWORD,adsmogo,REJECT
+DOMAIN-KEYWORD,adsrvmedia,REJECT
+DOMAIN-KEYWORD,adwords,REJECT
+DOMAIN-KEYWORD,adservice,REJECT
+DOMAIN-SUFFIX,appsflyer.com,REJECT
+DOMAIN-KEYWORD,domob,REJECT
+DOMAIN-SUFFIX,doubleclick.net,REJECT
+DOMAIN-KEYWORD,duomeng,REJECT
+DOMAIN-KEYWORD,dwtrack,REJECT
+DOMAIN-KEYWORD,avlyun,DIRECT
+DOMAIN-KEYWORD,guanggao,REJECT
+DOMAIN-KEYWORD,lianmeng,REJECT
+DOMAIN-SUFFIX,mmstat.com,REJECT
+DOMAIN-KEYWORD,mopub,REJECT
+DOMAIN-KEYWORD,omgmta,REJECT
+DOMAIN-KEYWORD,openx,REJECT
+DOMAIN-KEYWORD,partnerad,REJECT
+DOMAIN-KEYWORD,pingfore,REJECT
+DOMAIN-KEYWORD,supersonicads,REJECT
+DOMAIN-KEYWORD,uedas,REJECT
+DOMAIN-KEYWORD,umeng,REJECT
+DOMAIN-KEYWORD,usage,REJECT
+DOMAIN-SUFFIX,vungle.com,REJECT
+DOMAIN-KEYWORD,wlmonitor,REJECT
+DOMAIN-KEYWORD,zjtoolbar,REJECT
+DOMAIN-KEYWORD,google,PROXY
+
+IP-CIDR,192.168.0.0/16,DIRECT
+IP-CIDR,10.0.0.0/8,DIRECT
+IP-CIDR,172.16.0.0/12,DIRECT
+IP-CIDR,127.0.0.0/8,DIRECT
+GEOIP,CN,DIRECT
+FINAL,PROXY
+
+[Host]
+localhost = 127.0.0.1
+" >> ./ssc.tmp
