a9df41cf0b78c4a5250d63b6a60cfe0716382fb8
0xHO <0xHO>
2023-07-04 21:05:46 +0800

    add service.yaml

---
+++ service.sh
@@ -0,0 +1,53 @@
+service=$1
+filename=service/${2:-$1}.yaml
+
+# echo $service
+# echo $filename
+#   - PROCESS-NAME,tv.twitch.android.app
+#   - DOMAIN-SUFFIX,ext-twitch.tv
+#   - DOMAIN-SUFFIX,jtvnw.net
+#   - DOMAIN-SUFFIX,ttvnw.net
+
+function getdomain () {
+    # default file
+    include=$1
+    iscn=$2
+    echo $include
+    echo "   # ------ ${include} ------>" >> $filename
+    grepcn="-v"
+    if [[ ${iscn} == 1 ]] ;then
+        grepcn=""
+    fi
+    # DOMAIN
+    domains=`cat ./domainlist/data/${include}|grep "full:"|grep ${grepcn} "@cn"|grep -v "^#"|grep -v "@ads"|cut -d: -f2|cut -d" " -f1`
+    for domain in ${domains}
+    do
+        echo "   - DOMAIN,${domain}" >> $filename
+    done
+    # DOMAIN-SUFFIX
+    domainsuffixs=`cat ./domainlist/data/${include}|grep -v "full:"|grep -v "include:"|grep ${grepcn} "@cn"|grep -v "^#"|grep -v "@ads"|grep -v "regexp:"|cut -d" " -f1`
+    for domainsuffix in ${domainsuffixs}
+    do
+        echo "   - DOMAIN-SUFFIX,${domainsuffix}" >> $filename
+    done
+    # include
+    includs=`cat ./domainlist/data/${include}|grep "include:"|grep "#"|cut -d: -f2|cut -d" " -f1`
+    for incoude in ${includs}
+    do
+        getdomain ${incoude} ${iscn}
+    done
+}
+
+echo "# Service: ${service}
+# Repo: https://github.com/0xHO/WhiteList/blob/main/${filename}
+# Update: `date`
+payload:" > $filename
+
+for domain in ${service}
+do
+    if [[ ${domain: -3} == "@cn" ]]; then
+        getdomain ${domain:: -3} 1
+    else
+        getdomain ${domain} 0
+    fi
+done
